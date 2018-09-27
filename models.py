498c46f29c1b1a83b60ba82c8cba97a34536fa2b
jamesw8 <jameswong8@gmail.com>
2018-09-27 02:58:43 -0400

    Changed password uniqueness

--- models.py
+++ models.py
@@ -11,12 +11,15 @@ class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(100), unique=True, nullable=False)
 	email = db.Column(db.String(100), unique=True, nullable=False)
-	password = db.Column(db.String(100), unique=True, nullable=False)
+	password = db.Column(db.String(200), nullable=False)
 	
-	def __init__(self, username, email, password):
-		self.name = name.title(),
-		self.email = email.lower(),
-		self.password = generate_password_hash(password)
+	def __init__(self, name, email, password):
+		self.name = name.title()
+		self.email = email.lower()
+		self.password = self.hash_password(password)
+
+	def hash_password(self, password):
+		return generate_password_hash(password)
 
 	def check_password(self, password):
 		return check_password_hash(self.password, password)
