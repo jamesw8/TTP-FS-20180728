0733ce8c42ec63cef45f2ec909049d09127f53b9
jamesw8 <jameswong8@gmail.com>
2018-09-27 02:58:12 -0400

    Added user signup./login functionality

--- app.py
+++ app.py
@@ -1,37 +1,56 @@
-from flask import Flask, session, request, url_for, redirect, flash
+from flask import Flask, session, request, url_for, redirect, flash, render_template
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
 
 from models import db, User
+from forms import SignupForm, LoginForm
 
 app = Flask(__name__)
 app.secret_key = 'sekrit'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db.init_app(app)
+with app.app_context():
+	db.create_all()
 
 @app.route('/')
 def index():
 	"""
 	Landing page
 	"""
-	return 'Landing page'
+	return render_template('index.html', title='myPortfolio')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	"""
+	Login page with user querying
 	"""
+	if 'user' in session:
+		return redirect(url_for('portfolio'))
+
+	form = LoginForm()
 	if request.method == 'POST':
-		email, password = request.form.get('email', ''), request.form.get('password', '')
-		User.query.filter_by(email=email)
-		session['user'] = None # Look up sql query here
-	return 'Login with Email and Password'
+		print(form.validate())
+		print(form.errors)
+		if form.validate_on_submit():
+			email = form.email.data
+			password = form.password.data
+			user = User.query.filter_by(email=email).first()
+			print('Trying')
+			if user != None:
+				if user.check_password(password):
+					session['user'] = user.id
+					return redirect(url_for('trade'))	
+		flash('Email/Password combination does not exist.')
+	return render_template('login.html', form=form, title='Login')
 
 @app.route('/logout')
 def logout():
 	"""
-	Logout of website
+	Log out of website by removing user key from session
 	"""
 	if 'user' in session:
 		session.pop('user', None)
+
 	return redirect(url_for('index'))
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -40,28 +59,50 @@ def register():
 	User's cash account is $5000.00 USD by default
 	One account per email
 	"""
-	print(dir(request))
-	print(request.method)
-	if request.method == 'GET':
-		return '''
-		<form method="post">
-			<input type="text" name=name placeholder="Name" />
-			<input type="text" name=email placeholder="Email" />
-			<input type="password" name=password placeholder="Password" />
-			<input type="submit" value="Register"/>
-		</form>
-		'''
-	elif request.method == 'POST':
-		# Need to validate
-		pass
-	else:
-		return redirect(url_for('index'))
-	return 'Register with Name, Email, and Password'
+	if 'user' in session:
+		return redirect(url_for('portfolio'))
 
+	print(request.form)
+	form = SignupForm()
+	if request.method == 'POST':
+		if form.validate_on_submit():
+			name = form.name.data
+			email = form.email.data
+			password = form.password.data
+			# Create new User from model
+			new_user = User(name=name, email=email, password=password)
+			# Attempt to add new user to db
+			try:
+				db.session.add(new_user)
+				db.session.flush()
+			except IntegrityError:
+				db.session.rollback()
+				flash('Email is already in use.')
+				return render_template('register.html', form=form, title='Register')
+			else:
+				db.session.commit()
+
+			user = User.query.filter_by(email=email).first()
+			session['user'] = user.id
+			app.logger.info('Successful signup')
+			return redirect(url_for('trade'))
+
+	return render_template('register.html', form=form, title='Register')
+
+# TODO
+@app.route('/portfolio')
+def portfolio():
+	if 'user' not in session:
+		return redirect(url_for('login'))
+
+	return 'Portfolio'
+
+# TODO
 @app.route('/trade', methods=['GET'])
 def trade():
-	if 'username' not in session:
+	if 'user' not in session:
 		return redirect(url_for('login'))
+
 	return 'Trading page'
 
 if __name__ == '__main__':
